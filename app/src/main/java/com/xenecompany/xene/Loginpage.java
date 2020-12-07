68730da39f9e3bebca18e7e01ddf1f50c06ea19a
yug-ank <yuganksharma012@gmail.com>
2020-12-08 02:13:24 +0530

    login page

--- app/src/main/java/com/xenecompany/xene/Loginpage.java
+++ app/src/main/java/com/xenecompany/xene/Loginpage.java
@@ -2,13 +2,21 @@ package com.xenecompany.xene;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Loginpage extends AppCompatActivity {
+public class Loginpage extends Activity {
 
-    @Override
+    private EditText userNumber;
+    private Button sendOtpButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
+        userNumber=(EditText)findViewById(R.id.userNumber);
+        sendOtpButton=(Button)findViewById(R.id.sendotpbutton);
+
     }
 }
---
+++ app/src/main/java/com/xenecompany/xene/otp_page.java
@@ -0,0 +1,14 @@
+package com.xenecompany.xene;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otp_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_page);
+    }
+}
