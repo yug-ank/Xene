ce050c588ef5d3e1ef16b4b9679de981b1a13b65
sheshank-2903 <sheshanksharma2903@gmail.com>
2021-02-13 13:33:13 +0530

    final

--- app/src/main/java/com/xenecompany/xene/contact_us.java
+++ app/src/main/java/com/xenecompany/xene/contact_us.java
@@ -3,7 +3,6 @@ package com.xenecompany.xene;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,57 +23,9 @@ public class contact_us extends AppCompatActivity {
     Pattern phonePattern=Pattern.compile(phoneregex);
     Matcher emailMatcher;
     Matcher phoneMatcher;
-    boolean nameBoolean=false , contactBoolean=false , issueBoolean=false;
-    TextWatcher nameTextWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    boolean details=false;
+    TextWatcher nameTextWatcher , contactTextWatcher , issueTextWatcher;
 
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            if(name.getText().toString().trim().length()>0){
-                nameBoolean=true;
-            }
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-        }
-    };
-    TextWatcher contactTextWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            if(contact.getText().toString().trim().length()>0){
-                contactBoolean=true;
-            }
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-        }
-    };
-    TextWatcher issueTextWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            if(issue.getText().toString().trim().length()>0){
-                    issueBoolean=true;
-            }
-        }
-        @Override
-        public void afterTextChanged(Editable editable) {
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,22 +37,67 @@ public class contact_us extends AppCompatActivity {
         send=(Button)findViewById(R.id.contactUsButton);
         emailMatcher=emailPattern.matcher(contact.getText().toString().trim());
         phoneMatcher=phonePattern.matcher(contact.getText().toString().trim());
-        name.addTextChangedListener(nameTextWatcher);
-        contact.addTextChangedListener(contactTextWatcher);
-        issue.addTextChangedListener(issueTextWatcher);
-        if(nameBoolean && contactBoolean && issueBoolean){
-            Log.i("rectify" , "ffasas");
-            if(emailMatcher.matches() || phoneMatcher.matches()){
-                send.setEnabled(true);
-                send.setBackgroundResource(R.drawable.sign_up_button);
+        nameTextWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
-        }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(name.getText().toString().trim().length()>0){
+                    contactTextWatcher = new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                            if(contact.getText().toString().trim().length()>0){
+                                issueTextWatcher = new TextWatcher() {
+                                    @Override
+                                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                                    }
+
+                                    @Override
+                                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                                        if(issue.getText().toString().trim().length()>0){
+                                            if(emailMatcher.matches() || phoneMatcher.matches()) {
+                                                send.setEnabled(true);
+                                                send.setBackgroundResource(R.drawable.sign_up_button);
+                                                }
+                                        }
+                                    }
+                                    @Override
+                                    public void afterTextChanged(Editable editable) {
+                                    }
+                                };
+                                issue.addTextChangedListener(issueTextWatcher);
+
+                            }
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable editable) {
+                        }
+                    };
+                    contact.addTextChangedListener(contactTextWatcher);
+
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        };
+        name.addTextChangedListener(nameTextWatcher);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nameBoolean=false;
-                contactBoolean=false;
-                issueBoolean=false;
+                details=false;
                 String personname=name.getText().toString().trim();
                 String personcontact=contact.getText().toString().trim();
                 String personissue=issue.getText().toString().trim();
